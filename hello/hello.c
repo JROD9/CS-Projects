f1fdde2ae264d5f687f4b2b3ca1cbd1cbcd0cce8
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-17 01:32:10 +0000

    Thu, Nov 16, 2023, 8:32 PM -05:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    string name = get_string("what is your name?\n");
-    printf("hello,%s", name);
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
