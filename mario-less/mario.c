00d1f33bdbf62b584bd587f41b07c5dcf32b6eb3
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-17 22:01:24 +0000

    Fri, Nov 17, 2023, 5:01 PM -05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,7 +9,7 @@ int main(void)
     {
         height = get_int("how tall is your pyramid? \n");
     }
-    while ( height<0 && height>9);
+    while ( height>1 || height<=8);
 
 
     for (int rows = 0; rows < height; rows ++)
