ea518279128d6bbc8428f2f388638c67bc72d02f
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-17 02:19:43 +0000

    Thu, Nov 16, 2023, 9:19 PM -05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,9 +5,9 @@ int main(void)
 {
     int height;
         height = get_int("positive number\n");
-  for (int i = 0; i > 1; i++)
+  for (int i = 0; i > 1; i++);
   {
-      for (int j = 0; i < 8; i++)
+      for (int j = 0; i < 8; i++);
       {
           printf("#");
       }
