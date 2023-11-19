5a4e702b1f1de9a6e3ccaf70aebee91b4ded44bc
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-19 21:37:19 +0000

    Sun, Nov 19, 2023, 4:37 PM -05:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    int ssize;
     // TODO: Prompt for end size
-
+    int esize;
     // TODO: Calculate number of years until we reach threshold
-
+    
     // TODO: Print number of years
 }
