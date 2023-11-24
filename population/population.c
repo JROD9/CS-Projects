da0bd45baf9c6964d917ab6971c09de9a04c3e87
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-24 23:12:30 +0000

    Fri, Nov 24, 2023, 6:12 PM -05:00

--- population/population.c
+++ population/population.c
@@ -21,7 +21,7 @@ int main(void)
     // TODO: Calculate number of years until we reach threshold
 
      int years = 0;
-    while (esize >= ssize)
+    while (esize > ssize)
     {
         ssize = ssize + ssize / 3 - ssize / 4;
         years++;
