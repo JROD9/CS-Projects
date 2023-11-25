c24902159d7107aca151bc064839081375c16cc5
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-25 16:40:02 +0000

    Sat, Nov 25, 2023, 11:40 AM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 
 int main(void)
 {
-    {
         string text = get_string("Text: ");
 
         int letters = 0;
