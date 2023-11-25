9717849eb9fab36e0379cac2e73afb8d151e357c
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-25 16:51:35 +0000

    Sat, Nov 25, 2023, 11:51 AM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,20 +1,19 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <math.h>
-
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-        string text = get_string("Text: ");
-        int letters = 0;
-        int words = 1;
-        int sentences = 0;
+    string text = get_string("Text: ");
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
 
     for (int i = 0; i < strlen(text); i++)
     {
-        if(isalpha(text[i]))
+        if (isalpha(text[i]))
         {
             letters++;
         }
@@ -29,16 +28,16 @@ int main(void)
             sentences++;
         }
     }
-        float L = (float) letters / (float) words * 100;
-        float S= (float) sentences / (float) words * 100;
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
 
-        int index = round(0.0588 * L - 0.296 * S - 15.8);
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
-         if (index < 1)
+    if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(index > 16)
+    else if (index > 16)
     {
         printf("Grade 16+\n");
     }
@@ -47,37 +46,3 @@ int main(void)
         printf("Grade %i\n", index);
     }
 }
-
-
-
-    // analysing input
-
-   // int count_letters(string text);
-   // int count_words(string text);
-   // int count_sentences(string text);
-
-
-    // calculation input
- //   int index, L, S;
- //   L = (t/w);
- //   S = (e/w);
- //   index = 0.0588 * L - 0.296 * S - 15.8;
-
-
-    // printing grade
- //   if (index >= 1 && index < 16)
- //   {
- //       printf("Grade %i\n", index);
- //   }
- //   else if(index >= 16)
-//    { make readability
-// ./readability
- //       printf("Grade 16+");
- //   } hello oven baker mom. your the best. and the worst.
-  //  else (index < 1)
- //   {
- //       printf("Before Grade 1");
- //   }
-// {
-  //  printf("0");
-// }
