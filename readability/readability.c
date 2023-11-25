b5600806ee5501b4dc63a292fe4ec886c5099be7
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-25 15:51:13 +0000

    Sat, Nov 25, 2023, 10:51 AM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,33 +3,33 @@
 
 int main(void)
 {
-    do
+   // do
     {
         string text = get_string("Text: ");
-        printf(text);
+        printf("%s", text);
     }
-    while();
+   // while();
 
-    int index, L, S, w, t, e;
-    t = ;
-    w = ;
-    e = ;
-    L = (t/w);
-    S = (e/w);
-    index = 0.0588 * L - 0.296 * S - 15.8;
+   // int index, L, S, w, t, e;
+    //t = ;
+   // w = ;
+   // e = ;
+    //L = (t/w);
+   // S = (e/w);
+   // index = 0.0588 * L - 0.296 * S - 15.8;
 
 
     //printing grade
-    if (index >= 1 && index < 16)
-    {
-        printf("Grade %i\n", index);
-    }
-    else if(index >= 16)
-    {
-        printf("Grade 16+");
-    }
-    else (index < 1)
-    {
-        printf("Before Grade 1");
-    }
+  //  if (index >= 1 && index < 16)
+  //  {
+   //     printf("Grade %i\n", index);
+   // }
+  //  else if(index >= 16)
+  //  {
+  //      printf("Grade 16+");
+ //   }
+ //   else (index < 1)
+  //  {
+  //      printf("Before Grade 1");
+ //   }
 }
