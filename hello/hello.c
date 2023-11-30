0b9187c4ac72643316cf61773202138568200d56
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-11-30 23:31:08 +0000

    Thu, Nov 30, 2023, 6:31 PM -05:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,16 +3,7 @@
 
 int main(void)
 {
-    int apples;
-    apples = get_int("How old are you");
-
-    if (apples > 5)
-    {
     printf("Hello %i\n", apples);
-    }
-    else
-    {
-        printf("%i\n", apples);
-    }
 }
+
 
