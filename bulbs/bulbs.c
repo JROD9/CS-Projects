5756d127d8702e812609f7cafbd127243ce4704d
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-01 20:21:44 +0000

    Fri, Dec 1, 2023, 3:21 PM -05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -15,7 +15,7 @@ int main(void)
 
     for(int i = 0, n = strlen(name[1]); i < n; i++)
     {
-         printf("%c", toupper(name[i]));
+         printf("%c", toupper(n));
     }
     printf("\n");
 
