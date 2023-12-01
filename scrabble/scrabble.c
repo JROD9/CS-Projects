02f553f1399a35bfd4e1023367969406583fb3ba
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-01 21:55:32 +0000

    Fri, Dec 1, 2023, 4:55 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -20,11 +20,11 @@ int main(void)
 
     // TODO: Print the winner
     if(score1 > score2)
-        printf("Player 1 wins!");
+        printf("%s\n", "Player 1 wins!");
     else if(score1 < score2)
-        printf("Player 2 wins!");
+        printf("%s\n", "Player 2 wins!");
     else
-        printf("Tie!");
+        printf("%s\n", "Tie");
 
 }
 
