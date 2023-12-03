0b5ce7750ec826809f0afa7f5bbf7ebfa84679cd
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 18:28:11 +0000

    Sun, Dec 3, 2023, 1:28 PM -05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,14 +3,16 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    for (int i = 0; i < height; i++)
+    for (int row = 0; row < width; row++)
     {
-        for (int j = 0; j < width; j++)
+        for (int column = 0; column < height; column++)
         {
             // Make black pixels turn red
-            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            if (image[column][row].rgbtred == 0x00 &&
+            image[column][row].rgbtGreen == 0x00 &&
+            image[column][row].rgbtBlue == 0x00)
             {
-                image[i][j].rgbtRed = 0xff;
+                image[column][row].rgbtRed = 0xff;
                 return 0;
             }
         }
