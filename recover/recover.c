af534e332759867da212a4cd4a891a2a31ff48eb
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 21:45:56 +0000

    Sun, Dec 3, 2023, 4:45 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,15 +1,15 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
- if (argc != 2)
- {
-    printf("Usage: ./recover IMAGE\n");
-    return 1;
- }
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
     // open file for reading
     FILE *input_file = fopen(argv[1], "r");
 
@@ -20,41 +20,41 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    //storing blocks of 512 bytes in an array
+    // storing blocks of 512 bytes in an array
     unsigned char buffer[512];
 
-    //track number of images being created
+    // track number of images being created
     int count_image = 0;
 
-    //files pointer for recovered images
+    // files pointer for recovered images
     FILE *output_file = NULL;
 
-    //char filename[8]
+    // char filename[8]
     char *filename = malloc(8 * sizeof(char));
 
     while (fread(buffer, sizeof(char), 512, input_file))
     {
-    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-    {
-        // write the jped filenames
-        sprintf(filename, "%03i.jpg", count_image);
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // write the jped filenames
+            sprintf(filename, "%03i.jpg", count_image);
 
-        //open output_file for writing
-        output_file = fopen(filename, "w");
+            // open output_file for writing
+            output_file = fopen(filename, "w");
 
-        //count the number of images created so far
-        count_image++;
-    }
-    }
-    // check if the output has been used for valid input
-    if (output_file != NULL)
-    {
-        fwrite(buffer, sizeof(char), 512, output_file);
+            // count the number of images created so far
+            count_image++;
+        }
+
+        // check if the output has been used for valid input
+        if (output_file != NULL)
+        {
+            fwrite(buffer, sizeof(char), 512, output_file);
+        }
     }
     free(filename);
     fclose(output_file);
     fclose(input_file);
 
     return 0;
-
 }
