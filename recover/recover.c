8675c1b7d26d954f02abf596796c03d4262d651e
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 21:27:52 +0000

    Sun, Dec 3, 2023, 4:27 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -8,4 +10,31 @@ int main(int argc, char *argv[])
     printf("Usage: ./recover IMAGE\n");
     return 1;
  }
+
+    // open file for reading
+    FILE *input_file = fopen(argv[1], "r");
+
+    // chack for validility
+    if (input_file == NULL)
+    {
+        printf("could not open file");
+        return 2;
+    }
+
+    //storing blocks of 512 bytes in an array
+    unsigned car buffer[512];
+
+    //track number of images being created
+    int count_image = 0;
+
+    //files pointer for recovered images
+    FILE *output_file = NULL;
+
+    //char filename[8]
+    char *count_image = malloc(8 * sizeof(cahr));
+
+    fread(buffer, sizeof(char), 512, input_file);
+
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == oxff && buffer[3] & 0xf0 == 0xe0);
+
 }
