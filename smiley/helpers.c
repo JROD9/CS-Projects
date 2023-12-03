34c20bf4a8f86a63a307f1b01b3ee7b9106b477e
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 18:32:40 +0000

    Sun, Dec 3, 2023, 1:32 PM -05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -8,9 +8,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         for (int column = 0; column < height; column++)
         {
             // Make black pixels turn red
-            if (image[column][row].rgbtRed == 0x00 &&
-            image[column][row].rgbtGreen == 0x00 &&
-            image[column][row].rgbtBlue == 0x00)
+            if (image[column][row].rgbtRed == 0x00 && image[column][row].rgbtGreen == 0x00 && image[column][row].rgbtBlue == 0x00)
             {
                 image[column][row].rgbtRed = 0xff;
             }
