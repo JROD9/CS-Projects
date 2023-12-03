3a2f4c92da1f041b808ef26a138bdd294445fb0b
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 19:07:41 +0000

    Sun, Dec 3, 2023, 2:07 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,12 +26,40 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // comb through each row
     for(int i = 0; i < height; i++)
     {
+        // comb through each column
         for(int j = 0; j < width; j++)
         {
             // converting images into float
-            float 
+            float originalRed = image[i][j].rgbtRed;
+            float originalBlue = image[i][j].rgbtBlue;
+            float originalGreen = image[i][j].rgbtGreen;
+
+            // find the updated pixel value
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            //find the average value
+            if (sepiaRed > 255);
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaBlue > 255);
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+
+            //update the final pixels
+            image[i][j].rgbtRed = sepiaRed;
+            Image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
