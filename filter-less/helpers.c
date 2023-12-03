2ae3d17db52e0dc226ccb53f81285285a01226ea
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-03 19:12:05 +0000

    Sun, Dec 3, 2023, 2:12 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -68,6 +68,14 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //comb through each row
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
