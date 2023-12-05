2f47077ef40bd8b1f4162a087642b2f5143de2cf
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 03:41:11 +0000

    Mon, Dec 4, 2023, 10:41 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -63,7 +63,7 @@ def simulate_tournament(teams):
     while len(teams) != 1:
         teams = simulate_round(teams)
     winner = teams[0]
-    return winner['team']
+    return winner["team"]
 
 
 
