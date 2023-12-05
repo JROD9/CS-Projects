e604233b0279141ace23f3407f679e7824b4153f
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 03:35:03 +0000

    Mon, Dec 4, 2023, 10:35 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -35,12 +35,13 @@ def main():
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
 
-def simulate_game(team1, team2):
-    """Simulate a game. Return True if team1 wins, False otherwise."""
-    rating1 = team1["rating"]
-    rating2 = team2["rating"]
-    probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
-    return random.random() < probability
+def simulate_tournament(teams):
+    """Simulate a tournament. Return name of winning team."""
+    # TODO
+    while len(teams) != 1:
+        teams = simulate_round(teams)
+    winner = teams[0]
+    return winner["team"]
 
 
 def simulate_round(teams):
