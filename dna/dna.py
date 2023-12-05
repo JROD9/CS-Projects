9c1b1327a82d5deaed8a5243c30f01f459adcc69
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 19:14:40 +0000

    Tue, Dec 5, 2023, 2:14 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -27,6 +27,18 @@ def main():
         result[subsequence] = longest_match(dna_sequence, subsequence)
 
     # TODO: Check database for matching profiles
+    for person in database:
+        match = 0
+        for subsequence in subsequences:
+            if int(person[subsequence]) == result[subsequence]:
+                match += 1
+
+        if match == len(subsequences):
+            print(person["name"])
+            return
+
+    print("No Match")
+
 
     return
 
