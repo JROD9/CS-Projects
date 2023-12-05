d821266ba8b2bea3bffc0036065b3ef515c984d4
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 02:32:33 +0000

    Mon, Dec 4, 2023, 9:32 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -25,10 +25,12 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    while len(teams) > 1:
-        teams = simulate_round(teams)
-
-        return teams[0]["team"]
+    for i in range(N):
+        winner = simulate_tournament(teams)
+    if winner in counts:
+        counts[winner] += 1
+    else:
+        counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -60,12 +62,10 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    for i in range(N):
-        winner = simulate_tournament(teams)
-    if winner in counts:
-        counts[winner] += 1
-    else:
-        counts[winner] = 1
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+
+        return teams[0]["team"]
 
 
 if __name__ == "__main__":
