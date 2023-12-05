b2101f403dae8db440640087ee0cd4457104ebd3
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 03:28:32 +0000

    Mon, Dec 4, 2023, 10:28 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,22 +16,20 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    filename = sys.argv[1]
-
-    with open(filename) as file:
-        reader = csv.DictReader(file)
-        for team in reader:
-            team["rating"] = int(team["rating"])
-            teams.append(team)
+    f = open(sys.argv[1], "r")
+    handle = csv.DictReader(f)
+    for item in handle:
+        item['rating'] = int(item['rating'])
+        teams.append(item)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournament(teams)
-    if winner in counts:
-        counts[winner] += 1
-    else:
-        counts[winner] = 1
+        if winner['team'] in counts:
+            counts[winner['team']] += 1
+        else:
+            counts[winner['team']] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
