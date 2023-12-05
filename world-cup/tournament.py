83e81745d46ff5a807d9b11315e3eccebf66b953
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 04:02:20 +0000

    Mon, Dec 4, 2023, 11:02 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -11,21 +11,21 @@ def main():
 teams = []
     # TODO: Read teams into memory from file
 filename = sys.argv[1]
-    with open(filename) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
+with open(filename) as file:
+            reader = csv.DictReader(file)
+for row in reader:
             row["rating"] = int(row["rating"])
             teams.append(row)
 counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    for i in range(N):
+for i in range(N):
         winningTeam = simulate_tournament(teams)
-        if winningTeam in counts:
+if winningTeam in counts:
             counts[winningTeam] += 1
-        else:
+else:
             counts[winningTeam] = 1
 # Print each team's chances of winning, according to simulation
-    for team in sorted(counts, key=lambda team: counts[team], reverse=True):
+for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
@@ -37,12 +37,12 @@ def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
     winners = []
 # Simulate games for all pairs of teams
-    for i in range(0, len(teams), 2):
-        if simulate_game(teams[i], teams[i + 1]):
+for i in range(0, len(teams), 2):
+    if simulate_game(teams[i], teams[i + 1]):
             winners.append(teams[i])
-        else:
+    else:
             winners.append(teams[i + 1])
-return winners
+    return winners
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
