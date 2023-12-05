0506d661805dbc8fa267fe572d5ab999c28d85f6
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 05:23:00 +0000

    Tue, Dec 5, 2023, 12:23 AM -05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,7 +3,7 @@
 from cs50 import get_string
 
 # Prompt user for text
-text  = get_string("Text: ")
+text = get_string("Text: ")
 
 letters = 0
 words = 1
@@ -11,19 +11,19 @@ sentences = 0
 
 # Count number of letters, words, and sentences
 for i in text:
-    if i .isalpha():
+    if i.isalpha():
         letters += 1
     elif i == " ":
         words += 1
     elif i == "." or i == "!" or i == "?":
         sentences += 1
 
-#coleman-Liau Index
-index = 0.0588 * (letters/words*100) - 0.296 * (sentences/words*100) - 15.8
+# coleman-Liau Index
+index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
 if index < 1:
     print("Before Grade 1")
 elif index >= 16:
-    print("grade 16+")
+    print("Grade 16+")
 else:
     print("Grade ", round(index))
