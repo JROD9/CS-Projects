9ed0c13562da0f341350e806961a8072a1eb3192
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-05 19:04:36 +0000

    Tue, Dec 5, 2023, 2:04 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,10 +5,19 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
+    if len(sys.argv) != 3:
+        sys.exit("python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
-    
+    database = []
+    with open(sys.argv[1], 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            database.append(row)
+
     # TODO: Read DNA sequence file into a variable
+    with open(sys.argv[2], 'r') as file:
+        dna_sequence = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
 
