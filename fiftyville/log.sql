0947917a3c5ff4e5146c374c2124c639098fc721
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-06 00:36:01 +0000

    Tue, Dec 5, 2023, 7:36 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,18 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 select description from crime_scene_reports
-while year = 2021 and month = 7 and day = 28
+where year = 2021 and month = 7 and day = 28;
+
+select transcript from interviews
+where year = 2021 and month = 7 and day = 28 and transcript like "%bakery%";
+
+select bakery_security_logs.activity, bakery_security_logs.
+license_plate, people.name from people
+join bakery_security_logs on bakery_security_logs.license_plate =
+people.license_plate
+where bakery_security_logs.year = 2021
+and bakery_security_logs.month = 7
+and bakery_security_logs.day = 28
+and bakery_security_logs.hour = 10
+and bakery_security_logs.minute >= 15
+and bakery_security_logs <=25;
