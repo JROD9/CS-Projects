4e82ddc6e0d083d4b54dae4640435ff5864b2ad1
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-06 01:30:14 +0000

    Tue, Dec 5, 2023, 8:30 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -26,8 +26,8 @@ WHERE
   atm_transactions.year = 2021
   AND atm_transactions.month = 7
   AND atm_transactions.day = 28
-  AND atm_transactions.atm_location = 'Leggett Street'
-  AND atm_transactions.transaction_type = 'withdraw';
+  AND atm_location = "Leggett Street"
+  AND atm_transactions.transaction_type = "withdraw";
 
 -- Query 3
 SELECT caller, caller_name, receiver, receiver_name FROM phone_calls
