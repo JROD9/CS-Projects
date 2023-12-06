301b4753d6ef4794d2db070b557480b78bf3aaa0
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-06 01:07:30 +0000

    Tue, Dec 5, 2023, 8:07 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,10 +1,14 @@
 
-
 -- Query 1
-SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name
-FROM people
-JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-WHERE bakery_security_logs.year = 2021
+SELECT
+  bakery_security_logs.activity,
+  bakery_security_logs.license_plate,
+  people.name
+FROM
+  people
+  JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE
+  bakery_security_logs.year = 2021
   AND bakery_security_logs.month = 7
   AND bakery_security_logs.day = 28
   AND bakery_security_logs.hour = 10
@@ -12,42 +16,62 @@ WHERE bakery_security_logs.year = 2021
   AND bakery_security_logs.minute <= 25;
 
 -- Query 2
-SELECT people.name, atm_transactions.transaction_type
-FROM people
-JOIN bank_accounts ON bank_accounts.person_id = people.id
-JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
-WHERE atm_transactions.year = 2021
+SELECT
+  people.name,
+  atm_transactions.transaction_type
+FROM
+  people
+  JOIN bank_accounts ON bank_accounts.person_id = people.id
+  JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE
+  atm_transactions.year = 2021
   AND atm_transactions.month = 7
   AND atm_transactions.day = 28
   AND atm_transactions.atm_location = 'Leggett Street'
   AND atm_transactions.transaction_type = 'withdraw';
 
 -- Query 3
-SELECT caller, caller_name, receiver
-FROM phone_calls
-WHERE year = 2021
+SELECT
+  caller,
+  caller_name,
+  receiver
+FROM
+  phone_calls
+WHERE
+  year = 2021
   AND month = 7
   AND day = 28
   AND duration < 60;
 
+-- Update Query 1
+UPDATE flights
+SET origin_airport_id = airports.city
+FROM airports
+WHERE flights.origin_airport_id = airports.id;
 
-update flights
-set origin_airport_id = airports.city
-from airports
-where flights.origin_airport_id = airports.id;
+-- Update Query 2
+UPDATE
+  flights
+SET
+  destination_airport_id = airports.city
+FROM
+  airports
+WHERE
+  flights.destination_airport_id = airports.id;
 
-
-update flights
-set destination_airport_id = airports.city
-from airports
-where flights.destination_airport_id = airports.id;
-
-
-select id, hour, minute, origin_airport_id, destination_airport_id
-from flights
-where year = 2021
-and month = 7
-and day = 29
-order by hour asc
-limit 1;
-
+-- Query 4
+SELECT
+  id,
+  hour,
+  minute,
+  origin_airport_id,
+  destination_airport_id
+FROM
+  flights
+WHERE
+  year = 2021
+  AND month = 7
+  AND day = 29
+ORDER BY
+  hour ASC
+LIMIT 1;
