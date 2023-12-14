23eda4bd573973f5c18e4e8f8380ca11fd997911
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-14 20:05:12 +0000

    Thu, Dec 14, 2023, 3:05 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -41,45 +41,22 @@ def after_request(response):
     return response
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
+@app.route("/")
+@login_required
+def index():
+    totalBalance = balance = db.execute(
+        "SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+    portfolio = db.execute(
+        "SELECT symbol, SUM(quantity) FROM transactions WHERE u_id=? GROUP BY symbol", session["user_id"])
+    prices = []
 
-    # Forget any user_id
-    session.clear()
+    for i, owned in enumerate(portfolio):
+        temp = lookup(owned["symbol"])["price"]
+        prices.append(temp)
+        totalBalance += portfolio[i]["SUM(quantity)"] * prices[i]
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+    return render_template("index.html", port=portfolio, prices=prices, balance=balance, total_bal=totalBalance)
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        try:
-            # Query database for username
-            rows = db.execute("SELECT * FROM users WHERE username = ?",
-                              request.form.get("username"))
-
-            # Ensure username exists and password is correct
-            if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-                return apology("invalid username and/or password", 403)
-
-            # Remember which user has logged in
-            session["user_id"] = rows[0]["id"]
-
-            # Redirect user to home page
-            return redirect("/")
-
-        except Exception as e:
-            print(f"Exception during login: {e}")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
