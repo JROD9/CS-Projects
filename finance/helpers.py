f077613b706acac7160d2f9ba943d5bb89553531
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-28 16:42:39 +0000

    Thu, Dec 28, 2023, 11:42 AM -05:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import datetime
 import pytz
@@ -5,10 +6,30 @@ import requests
 import subprocess
 import urllib
 import uuid
-
-from flask import redirect, render_template, session
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from functools import wraps
+from werkzeug.security import check_password_hash, generate_password_hash
 
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Existing code...
+
+# The rest of your existing code...
+
+# Merged code with the new functions and imports...
 
 def apology(message, code=400):
     """Render message as an apology to user."""
@@ -24,7 +45,6 @@ def apology(message, code=400):
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
-
 def login_required(f):
     """
     Decorate routes to require login.
@@ -38,41 +58,12 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-
 def lookup(symbol):
     """Look up quote for symbol."""
-
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
-    try:
-        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
-        response.raise_for_status()
-
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        quotes.reverse()
-        price = round(float(quotes[0]["Adj Close"]), 2)
-        return {
-            "name": symbol,
-            "price": price,
-            "symbol": symbol
-        }
-    except (requests.RequestException, ValueError, KeyError, IndexError):
-        return None
-
+    # ... (your existing lookup function code)
 
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
+
+# ... (the rest of your existing code)
