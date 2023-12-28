02a2a37dd4778227465494dc3f86b21d83b84b63
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-28 16:30:59 +0000

    Thu, Dec 28, 2023, 11:30 AM -05:00

--- finance/app.py
+++ finance/app.py
@@ -187,7 +187,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-   session.clear()
+    session.clear()
 
     if request.method == "POST":
         # Ensure username was submitted
