cba6347637fa1a56607ba9f6624c644c361e85e2
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-28 16:40:57 +0000

    Thu, Dec 28, 2023, 11:40 AM -05:00

--- finance/app.py
+++ finance/app.py
@@ -1,8 +1,10 @@
+import os
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from werkzeug.security import generate_password_hash
-from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+
 from helpers import apology, login_required, lookup, usd
 
 app = Flask(__name__)
@@ -17,65 +19,6 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    session.clear()
-
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("Must provide username", 400)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("Must provide password", 400)
-
-        # Ensure password confirmation was submitted
-        elif not request.form.get("confirmation"):
-            return apology("Must confirm password", 400)
-
-        # Ensure password and confirmation match
-        elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("Passwords do not match", 400)
-
-        try:
-            # Start a transaction
-            with db.execute("BEGIN"):
-                # Query database for username
-                rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-                # Ensure username does not already exist
-                if len(rows) != 0:
-                    return apology("Username already exists", 400)
-
-                # Insert new user into the database
-                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
-                           request.form.get("username"), generate_password_hash(request.form.get("password")))
-
-                # Commit the transaction
-                db.execute("COMMIT")
-
-        except Exception as e:
-            # Log the exception for debugging
-            print(f"Error during registration: {e}")
-
-            # Rollback the transaction in case of an error
-            db.execute("ROLLBACK")
-
-            # Return an apology to the user
-            return apology("Error during registration", 500)
-
-        # Query database for the newly inserted user
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Remember which user has logged in session["user_id"] - assuming "id" is the column name in the users table
-        session["user_id"] = rows[0]["id"]
-
-        return redirect("/")
-    else:
-        return render_template("register.html")
 
 @app.after_request
 def after_request(response):
@@ -107,8 +50,6 @@ def index():
         "index.html", transactions=transactions, cash=cash[0]["cash"], account=account
     )
 
-if __name__ == "__main__":
-    app.run()
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
@@ -173,3 +114,147 @@ def history():
 def login():
     """Log user in"""
 
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        if not symbol:
+            return apology("missing symbol")
+
+        quote = lookup(symbol)
+        if not quote:
+            return apology("invalid symbol")
+
+        return render_template("quoted.html", quote=quote)
+    else:
+        return render_template("quote.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    session.clear()
+
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("Must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Must provide password", 400)
+
+        # Ensure password confirmation was submitted
+        elif not request.form.get("confirmation"):
+            return apology("Must confirm password", 400)
+
+        # Ensure password and confirmation match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords do not match", 400)
+
+        try:
+            # Start a transaction
+            with db.execute("BEGIN"):
+                # Query database for username
+                rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+                # Ensure username does not already exist
+                if len(rows) != 0:
+                    return apology("Username already exists", 400)
+
+                # Insert new user into the database
+                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
+                           request.form.get("username"), generate_password_hash(request.form.get("password")))
+
+                # Commit the transaction
+                db.execute("COMMIT")
+
+        except Exception as e:
+            # Log the exception for debugging
+            print(f"Error during registration: {e}")
+
+            # Rollback the transaction in case of an error
+            db.execute("ROLLBACK")
+
+            # Return an apology to the user
+            return apology("Error during registration", 500)
+
+        # Query database for the newly inserted user
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Remember which user has logged in session["user_id"] - assuming "id" is the column name in the users table
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    if request.method == "POST":
+        # ... (the rest of your sell route code)
+    else:
+        symbols = db.execute(
+            "SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol",
+            session["user_id"],
+        )
+        return render_template("sell.html", symbol=symbols)
+
+@app.route("/addcash", methods=["GET", "POST"])
+@login_required
+def addcash():
+    """User can add additional cash to their account"""
+    if request.method == "POST":
+        # ... (the rest of your addcash route code)
+    else:
+        return render_template("addcash.html")
+
+if __name__ == "__main__":
+    app.run()
