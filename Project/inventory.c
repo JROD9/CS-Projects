c1295d4f5d41256bee8de0383e1687625d7fe908
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-29 22:12:23 +0000

    Fri, Dec 29, 2023, 5:12 PM -05:00

--- Project/inventory.c
+++ Project/inventory.c
@@ -27,6 +27,9 @@ int main() {
     double prices[NUM_SALSAS] = {1.5, 2.0, 1.8, 3.0, 2.5}; // Prices for each fruit
     int inventory[NUM_SALSAS] = {INITIAL_INVENTORY, INITIAL_INVENTORY, INITIAL_INVENTORY, INITIAL_INVENTORY, INITIAL_INVENTORY};
 
+    printf("Welcome to the Fruit Sales Management System!\n");
+    printf("Please enter the number of fruits sold for each type of fruit (maximum 15).\n");
+
     getSales(fruitsold, salsaNames, inventory);
 
     int totalSales = 0;
@@ -37,7 +40,7 @@ int main() {
     printf("-----------------\n");
     for (int i = 0; i < NUM_SALSAS; ++i) {
         double revenue = fruitsold[i] * prices[i];
-        //Profits are 50%
+        // Profits are 50%
         double profit = revenue - (fruitsold[i] * 0.5);
         printf("%s: %d fruits | Inventory Left: %d | Revenue: $%.2f | Profit: $%.2f\n", salsaNames[i], fruitsold[i], inventory[i], revenue, profit);
 
