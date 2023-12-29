613c21a74181b3fa91ab7204be06cfb968c2dd4d
jordan rodriguez <119002599+JROD9@users.noreply.github.com>
2023-12-29 03:08:29 +0000

    Thu, Dec 28, 2023, 10:08 PM -05:00

--- finance/app.py
+++ finance/app.py
@@ -35,7 +35,19 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    #get user's stocks and shares
+    stocks = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0",
+                        user_id=session["user_id"])
+
+    #get users cash balence
+    cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+
+    #initialize variables for tot values
+    total_value = cash
+    grand_total = cash
+
+    #iterate over stocks and add price and tot value
+    
 
 
 @app.route("/buy", methods=["GET", "POST"])
